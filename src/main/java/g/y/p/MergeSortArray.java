59cf29e34f4b5f2deed243cc3af2f4be7d1dd17a
William Guo <guoiii@icloud.com>
2017-05-29 14:07:58 +0800

    inclusive vs exclusive

--- src/main/java/g/y/p/MergeSortArray.java
+++ src/main/java/g/y/p/MergeSortArray.java
@@ -6,13 +6,20 @@ import static java.util.Arrays.copyOfRange;
 
 public class MergeSortArray {
 
+//    static int[] sort(int[] ar) {
+//        int m = l + (h-l)/2;
+//        a[l,m]
+//        a[m+1,h]
+//
+//    }
+
     static int[] sort(int[] ar) {
         if (ar.length == 1 ) {
             return ar;
         }
         int mid = ar.length / 2;
-        int[] ar1 = copyOfRange(ar, 0, mid);
-        int[] ar2 = copyOfRange(ar, mid, ar.length);
+        int[] ar1 = copyOfRange(ar, 0, mid);//mid is exclusive
+        int[] ar2 = copyOfRange(ar, mid, ar.length);//mid is inclusive
         return merge(sort(ar1), sort(ar2));
     }
 
@@ -37,7 +44,7 @@ public class MergeSortArray {
 
 
     public static void main(String[] args) {
-        int[] a = {1,2,7,3,5,6,8,9,10,11};
+        int[] a = {1,3,7,2,4,6,8,9,10,5};
         int mid = a.length /2;
         System.out.println(Arrays.toString(copyOfRange(a,0,mid)));
         System.out.println(Arrays.toString(copyOfRange(a,mid,a.length)));
