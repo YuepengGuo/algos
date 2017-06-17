7ff38a491e833d4ec9e1d3d28e1528d0ea29355a
William Guo <guoiii@icloud.com>
2017-06-17 17:34:01 +0800

    update binary search with duplicated records

--- src/main/java/g/y/p/App.java
+++ src/main/java/g/y/p/App.java
@@ -1,5 +1,10 @@
 package g.y.p;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.SortedMap;
+import java.util.TreeMap;
+
 /**
  * Hello world!
  *
@@ -8,24 +13,59 @@ public class App
 {
     public static void main( String[] args )
     {
-    	int x = 1;
-    	int y = 2;
-    	System.out.println(x+" : "+y);
-    	System.out.println(Integer.toBinaryString(x));
-    	System.out.println(Integer.toBinaryString(y));
-    	
-    	System.out.println((x&y) + " : "+Integer.toBinaryString(x&y));
-    	System.out.println((x|y)+" : "+Integer.toBinaryString(x|y));
-    	System.out.println((x^y) +" : "+Integer.toBinaryString(x^y));
+//		String input = "This is a test sentence with the word 'this' appearing twice and 'is' is here thrice.";
+//		Map<String,Integer> result = exam2(input);
+//		System.out.println(result);
 
-        System.out.println(2 >>>1);
-		System.out.println(2 << 3);
+		TreeMap<String,Integer> map = new TreeMap<>();
 
-		String input = "saveChangesInTheEditor";
-		String[] tokens = input.split("[A-Z]");
+		map.put("this",2);
+		map.put("sentence",3);
+		map.put("twice",1);
+		map.put("gg",5);
 
-		x = 3;
+		System.out.println(map);
+		System.out.println(map.firstEntry());
+		for(Map.Entry<String,Integer> entry : map.entrySet()){
+			System.out.println(entry.getKey()+"->"+entry.getValue());
+		}
+	}
+	public static Map<String,Integer> exam(String input){
+		String separator = "\\s";
+		String puncs = "\\p{Punct}";
+		String[] tokens = input.replaceAll(puncs,"").split(separator);
+		Map<String,Integer> map = new HashMap<>();
+
+		for(int i=0;i<tokens.length;i++){
+			String token = tokens[i];
+			if(map.containsKey(token)){
+				Integer preValue = map.get(token);
+				map.put(token,preValue+1);
+			}else{
+				map.put(token,1);
+			}
+		}
+
+		return map;
+	}
+
+	public static Map<String,Integer> exam2(String input){
+		String separator = "\\s";
+		String puncs = "\\p{Punct}";
+		String[] tokens = input.replaceAll(puncs,"").split(separator);
+		SortedMap<String,Integer> map = new TreeMap<>();
 
 
+		for(int i=0;i<tokens.length;i++){
+			String token = tokens[i];
+			if(map.containsKey(token)){
+				Integer preValue = map.get(token);
+				map.put(token,preValue+1);
+			}else{
+				map.put(token,1);
+			}
+		}
+
+		return map;
 	}
 }
--- src/main/java/g/y/p/BinarySearch.java
+++ src/main/java/g/y/p/BinarySearch.java
@@ -1,7 +1,17 @@
 package g.y.p;
 
 import java.util.Arrays;
+import java.util.Comparator;
 
+/**
+ * <pre>
+ * low+1,
+ * low<high
+ * converge to low == high
+ * duplicated!!!
+ * the other direction first!!!
+ * </pre>
+ */
 public class BinarySearch {
     public static int rank(int key, int[] a){
         int l = 0;
@@ -16,11 +26,120 @@ public class BinarySearch {
         return -1;
     }
 
+
+    /**
+     * low+1,
+     * low<high
+     * converge to low == high
+     * the other direction first
+     *
+     */
+    public static int leftMost_desc(int[]a,int l,int h,int x){
+        int m ;
+        while(l<h){
+            m = l +(h-l)/2;
+            if(a[m]>x){// right half first
+                l = m+1;
+            }else{
+                h = m;
+            }
+        }
+        System.out.println("left at "+l+ " right at " +h);
+        return l;
+    }
+
+
+    /**
+     * low+1,
+     * low<high
+     * converge to low == high
+     * the other direction first
+     *
+     */
+    public static int leftMost_asc(int[]a,int l,int h,int x){
+        int m ;
+        while(l<h){
+            m = l +(h-l)/2;
+            if(a[m]<x){// right half first
+                l = m+1;
+            }else{
+                h = m;
+            }
+        }
+        System.out.println("left at "+l+ " right at " +h);
+        return l;
+    }
+    /**
+     * low+1,
+     * low<high
+     * converge to low == high
+     * the other direction first
+     *
+     */
+    public static int rightMost_desc(int[]a,int l,int h,int x){
+        int m ;
+        while(l<h){
+            m = l +(h-l)/2;
+            if(a[m]<x){//left part first
+                h = m;
+            }else{
+                l = m+1;
+            }
+        }
+        System.out.println("left at "+l+ " right at " +h);
+        return l;
+    }
+
+    /**
+     * low+1,
+     * low<high
+     * converge to low == high
+     * the other direction first
+     *
+     */
+    public static int rightMost_asc(int[]a,int l,int h,int x){
+        int m ;
+        while(l<h){
+            m = l +(h-l)/2;
+            if(a[m]>x){//left part first
+                h = m;
+            }else{
+                l = m+1;
+            }
+        }
+        System.out.println("left at "+l+ " right at " +h);
+        return l;
+    }
+
     public static void main(String[] args) {
-        int[] list = {2,3,6,1,4,9,8,7,5};
+        Comparator<Integer> comparator = new Comparator<Integer>() {
 
-        Arrays.sort(list);
+            @Override
+            public int compare(Integer o1, Integer o2) {
+                return o2.compareTo(o1);
+            }
+        };
 
-        System.out.println(rank(1,list));
+        // option 1
+        Integer[] array = new Integer[]{1, 24, 4, 4, 23,4,12};
+        Arrays.sort(array, comparator);
+
+//        System.out.println(Arrays.toString(array));
+        //         0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21
+        int[] d = {5,5,5,5,4,4,4,3,3,3,2 ,1 ,1 ,1 ,1 ,0 ,0 ,0 ,0 ,0 ,0 ,0 };
+
+        //         0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21
+        int[] a = {0,0,0,0,0,0,0,1,1,1,1 ,2 ,3 ,3 ,3 ,4 ,4 ,4 ,5 ,5 ,5 ,5 };
+
+//        System.out.println(leftMost(d,0,d.length-1,2));
+
+        System.out.println(leftMost_desc(d,0,d.length-1,1));
+
+        System.out.println(rightMost_desc(d,0,d.length-1,1));
+
+        System.out.println(leftMost_asc(a,0,a.length-1,3));
+
+        System.out.println(rightMost_asc(a,0,a.length-1,3));
+
     }
 }
